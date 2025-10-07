ac7cb0ecbcbc79fb6a76398ecd9d024e7663ee27
felipe <FelipeCorreaLemos@WIN-D8I71GMGMD7>
2025-10-07 17:43:27 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp4/Components/model/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp4.Components.model
+{
+    public class Cliente
+    {
+        public string nome { get; set; } = string.Empty;
+        public string cpf { get; set; } = string.Empty;
+        public string telefone { get; set; } = string.Empty;
+
+        public static Cliente UltimoCliente { get; set; }
+
+    }
+
+}
