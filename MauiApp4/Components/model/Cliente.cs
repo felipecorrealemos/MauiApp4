d91c0d3c1f0bed33d17924dbb418e6d3afb81d07
felipe <FelipeCorreaLemos@WIN-D8I71GMGMD7>
2025-10-22 20:56:08 -0300

    ......

--- MauiApp4/Components/model/Cliente.cs
+++ MauiApp4/Components/model/Cliente.cs
@@ -8,7 +8,6 @@ namespace MauiApp4.Components.model
 {
     public class Cliente
     {
-        public int id { get; set; }
         public string nome { get; set; } = string.Empty;
         public string cpf { get; set; } = string.Empty;
         public string telefone { get; set; } = string.Empty;
