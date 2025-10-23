d9c3c25c613f494420e5fea960ebb327c6e14902
felipe <FelipeCorreaLemos@WIN-D8I71GMGMD7>
2025-10-23 11:22:01 -0300

    .....

--- MauiApp4/Components/DAO/ClienteDAO.cs
+++ MauiApp4/Components/DAO/ClienteDAO.cs
@@ -22,7 +22,7 @@ namespace MauiApp4.Components.DAO
                 await conn.OpenAsync();
 
                 string sql = "INSERT INTO tb_cliente (nome, cpf, telefone) VALUES(@nome, @cpf, @telefone)";
-                
+
                 await using var cmd = new MySqlCommand(sql, conn);
                 cmd.Parameters.AddWithValue("@nome", novocliente.nome);
                 cmd.Parameters.AddWithValue("@cpf", novocliente.cpf);
@@ -53,7 +53,7 @@ namespace MauiApp4.Components.DAO
 
                 string sql = "SELECT * FROM tb_cliente";
 
-                await using var cmd = new MySqlCommand( sql, conn);
+                await using var cmd = new MySqlCommand(sql, conn);
 
                 await using var reader = await cmd.ExecuteReaderAsync();
 
@@ -61,6 +61,7 @@ namespace MauiApp4.Components.DAO
                 {
                     var cliente = new Cliente()
                     {
+                        id = reader.GetInt32(0),
                         nome = reader.GetString(1),
                         cpf = reader.GetString(2),
                         telefone = reader.GetString(3)
@@ -74,10 +75,100 @@ namespace MauiApp4.Components.DAO
 
             catch (Exception ex)
             {
-               return new List<Cliente>();
+                return new List<Cliente>();
             }
         }
 
-      
+        public async Task ExcluirCliente(int id)
+        {
+            try
+            {
+                // string de conexão
+                string connectionString = "server=localhost;user=root;password=root;database=db_empresa_one;";
+
+                await using var conn = new MySqlConnection(connectionString);
+
+                await conn.OpenAsync();
+
+                string sql = "Delete FROM tb_cliente WHERE id= @id";
+
+                await using var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+            catch
+            {
+
+            }
+
+        }
+
+        public async Task<Cliente?> BuscarClientePorID(int id)
+        {
+            try
+            {
+                string connectionString = "server=localhost;user=root;password=root;database=db_empresa_one;";
+
+                await using var conn = new MySqlConnection(connectionString);
+                await conn.OpenAsync();
+
+                string sql = "SELECT * FROM tb_cliente WHERE id = @id";
+
+                await using var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                await using var reader = await cmd.ExecuteReaderAsync();
+                await reader.ReadAsync();
+
+                Cliente cliente = new Cliente()
+                {
+                    id = reader.GetInt32(0),
+                    nome = reader.GetString(1),
+                    cpf = reader.GetString(2),
+                    telefone = reader.GetString(3),
+                };
+
+                return cliente;
+            }
+
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+
+
+        public async Task AtualizarCliente(Cliente cliente)
+        {
+            try
+            {
+                // string de conexão
+                string connectionString = "server=localhost;user=root;password=root;database=db_empresa_one;";
+
+                await using var conn = new MySqlConnection(connectionString);
+
+                await conn.OpenAsync();
+
+                string sql = "UPDATE tb_cliente SET nome=@nome, cpf=@cpf, telefone=@telefone WHERE id = @id";
+
+                await using var cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", cliente.id);
+                cmd.Parameters.AddWithValue("@nome", cliente.nome);
+                cmd.Parameters.AddWithValue("@cpf", cliente.cpf);
+                cmd.Parameters.AddWithValue("@telefone", cliente.telefone);
+
+                await cmd.ExecuteNonQueryAsync();
+
+                //return rows > 0;
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
     }
 }
--- MauiApp4/Components/model/Cliente.cs
+++ MauiApp4/Components/model/Cliente.cs
@@ -8,6 +8,7 @@ namespace MauiApp4.Components.model
 {
     public class Cliente
     {
+        public int id { get; set; } = 0;
         public string nome { get; set; } = string.Empty;
         public string cpf { get; set; } = string.Empty;
         public string telefone { get; set; } = string.Empty;
